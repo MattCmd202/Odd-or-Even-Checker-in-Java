542a7101ee335b6ce13ef3b14f9a5f45778a99d4
Matt <63430359+MattCmd202@users.noreply.github.com>
2025-05-20 00:17:36 +0800

    EvenOrOddChecker.java

--- EvenOrOddChecker.java
+++ EvenOrOddChecker.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class EvenOrOddChecker {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scnr = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        int number = scnr.nextInt();
 
         if (number % 2 == 0) {
             System.out.println(number + " is even.");
